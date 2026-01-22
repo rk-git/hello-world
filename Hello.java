7e89f883ec10c7f09b9e24e513c5410d1ab91e5d
Rk Somasundaram <rk@tigase.net>
2026-01-21 22:24:32 -0800

    PR polish: merge animation, toast, header glow

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
 
 public class Hello {
+  
   public static void main(String[] args) {
        System.out.println("Hello World!")
+  }
+
+  public static void print(String s) {
+     System.out.println("%s", s);
+     System.out.flush();
   }
 }
